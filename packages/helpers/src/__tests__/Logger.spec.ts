11649358d5dd8bc984b3382aefba71e70f9bf501
Erik Nielsen <erik@312development.com>
2023-02-04 14:56:18 -0600

    Added "scope" to Logger config (#620)

--- packages/helpers/src/__tests__/Logger.spec.ts
+++ packages/helpers/src/__tests__/Logger.spec.ts
@@ -1,4 +1,4 @@
-import log from 'signale';
+import signale from 'signale';
 import Logger from '../Logger';
 
 jest.mock('signale');
@@ -6,17 +6,29 @@ jest.mock('signale');
 const msg = 'howdy';
 
 describe('Logger', () => {
+  describe('scope', () => {
+    it('should set scope', () => {
+      new Logger({ scope: 'test' });
+      expect(signale.scope).toHaveBeenCalledWith('test');
+    });
+
+    it('should not set scope', () => {
+      new Logger();
+      expect(signale.scope).not.toHaveBeenCalled();
+    });
+  });
+  
   describe('debug', () => {
     it('should print to log when debug is true', () => {
       const cli = new Logger({ debug: true });
       cli.debug(msg);
-      expect(log.debug).toHaveBeenCalledWith(msg);
+      expect(signale.debug).toHaveBeenCalledWith(msg);
     });
 
     it('should not print to log when debug is false', () => {
       const cli = new Logger();
       cli.debug(msg);
-      expect(log.debug).not.toHaveBeenCalled();
+      expect(signale.debug).not.toHaveBeenCalled();
     });
   });
 
@@ -26,13 +38,13 @@ describe('Logger', () => {
       it('should print to log when quiet is false', () => {
         const cli = new Logger();
         cli[method](msg);
-        expect(log[method]).toHaveBeenCalledWith(msg);
+        expect(signale[method]).toHaveBeenCalledWith(msg);
       });
 
       it('should not print to log when quiet is true', () => {
         const cli = new Logger({ quiet: true });
         cli[method](msg);
-        expect(log[method]).not.toHaveBeenCalled();
+        expect(signale[method]).not.toHaveBeenCalled();
       });
     }
   );
