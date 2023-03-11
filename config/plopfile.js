25826450dd61825d26c9e7b21a8253366d9adb77
Erik Nielsen <erik@312development.com>
2023-03-11 15:14:24 -0600

    added npm install plop action

--- config/plopfile.js
+++ config/plopfile.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const fg = require('fast-glob');
 const uniq = require('lodash/uniq');
+const kebabCase = require('lodash/kebabCase');
+const cp = require('child_process');
 const { version } = require('../lerna.json');
 
 const templates = path.resolve(__dirname, '../.bin/templates');
@@ -8,8 +10,34 @@ const templates = path.resolve(__dirname, '../.bin/templates');
 module.exports = (plop) => {
   /** @type {import('plop').NodePlopAPI} */
 
-  plop.setGenerator('controller', {
-    description: 'application controller logic',
+  plop.setActionType('npmInstall', function (_, config) {
+    const {
+      dependencies = [],
+      devDependencies = [],
+      flags = [],
+      path: cwd = process.cwd(),
+    } = config;
+    const root = path.resolve(__dirname, '..');
+    const args = ['install', ...flags];
+    if (dependencies) {
+      args.push(...dependencies, '--save');
+    } else if (devDependencies) {
+      args.push(...devDependencies, '--save-dev');
+    }
+    args.push('--workspace', path.relative(root, cwd));
+    console.log(`npm ${args.join(' ')}`);
+    return new Promise((resolve, reject) => {
+      cp.execFile('npm', args, { cwd: root }, (error, stdout, stderr) => {
+        if (error) {
+          return reject(stderr);
+        }
+        resolve(stdout);
+      });
+    });
+  });
+
+  plop.setGenerator('generator', {
+    description: 'Generate a new package',
     prompts: [
       {
         type: 'list',
@@ -45,8 +73,6 @@ module.exports = (plop) => {
     actions: (data) => {
       console.log(data);
       const packages = path.resolve(__dirname, '..', `${data.type}s`);
-      // console.log(packages);
-      // return [];
       const files = fg
         .sync([path.join(templates, '**/*.hbs')], { dot: true })
         .filter((file) => !file.endsWith('cli.js.hbs'));
@@ -78,12 +104,19 @@ module.exports = (plop) => {
       });
 
       if (data.cli) {
-        actions.push({
-          type: 'add',
-          path: path.join(packages, '{{kebabCase name}}/cli.js'),
-          templateFile: path.join(templates, 'cli.js.hbs'),
-          abortOnFail: true,
-        });
+        actions.push(
+          {
+            type: 'add',
+            path: path.join(packages, '{{kebabCase name}}/cli.js'),
+            templateFile: path.join(templates, 'cli.js.hbs'),
+            abortOnFail: true,
+          },
+          {
+            type: 'npmInstall',
+            path: path.join(packages, kebabCase(data.name)),
+            dependencies: ['commander'],
+          }
+        );
       }
 
       console.log(actions);
