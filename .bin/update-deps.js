d3040a397265d0d99a0fc02f838c3bdb9380394b
Erik Nielsen <erik@312development.com>
2023-03-12 17:05:04 -0500

    updated update-deps script

--- .bin/update-deps.js
+++ .bin/update-deps.js
@@ -1,66 +1,72 @@
 #!/usr/bin/env node
 const path = require('path');
 const fs = require('fs');
-const exec = require('@nielse63/exec');
+const fg = require('fast-glob');
+const { default: exec } = require('@nielse63/exec');
 const log = require('./helpers/log');
 
 const dryRun = process.argv.includes('--dry-run');
+const root = path.resolve(__dirname, '..');
 
-const updateDependencies = async (
-  object = {},
-  flags = [],
-  options = { cwd: process.cwd() }
-) => {
-  const keys = Object.keys(object);
-  if (!keys.length) return;
-  const string = keys.map((key) => `${key}@latest`).join(' ');
-  const command = `npm install ${flags.join(' ')} ${string}`;
-  log(`Running ${command}`);
+const runCommand = async (flags) => {
+  const cmd = `npm install ${flags.join(' ')}`;
+  log(cmd);
   if (!dryRun) {
-    await exec(command, { ...options });
+    exec(cmd);
   }
 };
 
-const main = async () => {
-  const root = path.resolve(__dirname, '..');
-  const packages = path.join(root, 'packages');
-  const dirs = await fs.promises.readdir(packages);
-  const validPackages = dirs
-    .map((dir) => path.join(packages, dir))
-    .filter((abspath) => {
-      const stat = fs.statSync(abspath);
-      return (
-        stat.isDirectory() && fs.existsSync(path.join(abspath, 'package.json'))
-      );
-    });
-  const promises = [root, ...validPackages].map(async (dir) => {
-    const packageJsonContent = await fs.promises.readFile(
-      path.join(dir, 'package.json'),
-      'utf-8'
-    );
-    const json = JSON.parse(packageJsonContent);
-    if ('dependencies' in json) {
-      const flags = dir.includes('/packages/')
-        ? ['--workspace', path.relative(root, dir)]
-        : [];
-      // console.log({ dependencies: true, dir, flags });
-      await updateDependencies(json.dependencies, flags);
-    }
-    if ('devDependencies' in json) {
-      const flags = dir.includes('/packages/')
-        ? ['--workspace', path.relative(root, dir), '--save-dev']
-        : ['--save-dev'];
-      await updateDependencies(json.devDependencies, flags);
-    }
-  });
-  await Promise.all(promises);
+const updateDependencies = async ({
+  workspace,
+  dependencies = [],
+  devDependencies = [],
+}) => {
+  const flags = [];
+  if (workspace) {
+    flags.push('--workspace', workspace);
+  }
+  if (dependencies.length) {
+    await runCommand([...flags, '--save', ...dependencies]);
+  }
+  if (devDependencies.length) {
+    await runCommand([...flags, '--save-dev', ...devDependencies]);
+  }
 };
 
-main()
-  .then(() => {
-    log('Dependencies updated');
-  })
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
+const parseDependenciesObject = (object) => {
+  const deps = Object.entries(object).reduce((output, [key, value]) => {
+    if (!value.startsWith('file:')) {
+      return [...output, key];
+    }
+    return [...output];
+  }, []);
+  return deps;
+};
+
+const updateFromFile = async (filepath) => {
+  const content = await fs.promises.readFile(filepath, 'utf-8');
+  const json = JSON.parse(content);
+  const workspace = path
+    .dirname(path.relative(root, filepath))
+    .replace('.', '');
+  const output = { workspace, dependencies: [], devDependencies: [] };
+  ['dependencies', 'devDependencies'].forEach((key) => {
+    if (key in json) {
+      output[key] = parseDependenciesObject(json[key]);
+    }
   });
+  return output;
+};
+
+const main = async () => {
+  const files = await fg(['**/package.json'], {
+    deep: 3,
+    cwd: root,
+    ignore: ['node_modules'],
+  });
+  const paths = files.map((file) => path.join(root, file));
+  const objects = await Promise.all(paths.map(updateFromFile));
+  await Promise.all(objects.map(updateDependencies));
+};
+
+main().catch(console.error);
