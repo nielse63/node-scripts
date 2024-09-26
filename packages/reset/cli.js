46356972b57e932967b09cee4d5810117478c314
Erik Nielsen <erik@312development.com>
2024-09-26 04:39:31 -0400

    test: added tests to reset (#726)
    * test: added tests to reset
    
    * chore(deps): updated reset dependencies
    
    * chore: fixing dependency links

--- packages/reset/cli.js
+++ packages/reset/cli.js
@@ -6,7 +6,6 @@ const { default: reset } = require('./dist');
 program
   .option('--paths <paths...>', 'paths to remove')
   .option('--config [config]', 'config file to use', 'reset.config.js')
-  // .option('--gitignore', 'use .gitignore as a source of paths to remove')
   .description(pkg.description)
   .version(pkg.version)
   .parse(process.argv);
